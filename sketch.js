0c5d9b3c7cec659f16f3672192deecbf37d1c243
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-06-15 22:26:31 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,58 @@
+var speech;
+let palavra = " favor começar.  números de 0 a 9, esquerda, direita, cima, baixo, em Inglês";
+let conf;
 console.log('ml5 version: ', ml5.version)
 
 let soundClassifier;
-let resultP;
 
 function preload() {
   let prob = {
     probabilityThreshold: 0.95
   };
   soundClassifier = ml5.soundClassifier('SpeechCommands18w', prob);
-
 }
+
 
 function setup() {
   noCanvas();
   createP('Diga, em Inglês, números de 0 a 9 ou as palavras up, down, left, right, yes, no, stop, com um intervalo entre elas de 1 a dois segundos...Ao lado da palavra reconhecida, aparece o grau de confiança no reconhecimento da máquina.');
   soundClassifier.classify(gotResults);
+
+  speech = new p5.Speech();
+
+  speech.onStart = voiceReady();
 }
 
 function gotResults(error, results) {
   if (error) {
     console.log('erro : ', error);
   }
-  console.log(results[0].label, results[0].confidence);  
-  createP(results[0].label);
-  createP('confiança: ' + results[0].confidence);
-  if (results[0].label == 'stop'){
-    console.log('Vocè pediu pra parar...desligando');
-    stop();
- }   
+  palavra = results[0].label;
+  conf = results[0].confidence;
+  console.log(palavra, conf);
+  createP(palavra);
+  createP('confiança: ' + conf);
+  if (palavra == 'two') {
+    palavra = 'dois';
+    speech.speak(palavra);
+  } else {
+    speech.speak(palavra);
+  }
+
 }
 
-function draw() {
-  background(220);
+function voiceReady() {
+
+  console.log(speech.voices);
+
+  //let idioma = random(0,1);                                 
+  speech.setPitch(0.5);
+
+  //speech.setLang('en-US');
+
+  speech.setLang('pt-BR');
+
+  speech.speak(palavra);
+
+  //console.log(speech.voices);
 }
